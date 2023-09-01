1f61069e8a8db3ca8bf00341e7941b5103983e62
thething <atillzcomputers@gmail.com>
2023-09-01 10:20:01 -0400

    Create envierment.py

---
+++ envierment.py
@@ -0,0 +1,8 @@
+# Create a new virtual environment
+python3 -m venv twilio_env
+
+# Activate the virtual environment
+source twilio_env/bin/activate
+
+# Install Flask and Twilio's Python SDK
+pip install Flask twilio
